4ed17096f40cb678d4663218e6188f2067144c75
MalekHAMID <53518848+MalekHAMID@users.noreply.github.com>
2025-03-01 00:22:27 +0300

    Update marker_generator.dart

--- lib/src/components/marker_generator.dart
+++ lib/src/components/marker_generator.dart
@@ -23,29 +23,35 @@ class _MarkerGeneratorState extends State<MarkerGenerator> {
   List<GlobalKey> globalKeys = [];
   List<WidgetMarker> lastMarkers = [];
 
-  Future<Marker> _convertToMarker(GlobalKey key) async {
-    RenderRepaintBoundary boundary =
-        key.currentContext!.findRenderObject()! as RenderRepaintBoundary;
-    final image = await boundary.toImage(pixelRatio: 2);
-    final byteData =
-        await image.toByteData(format: ImageByteFormat.png) ?? ByteData(0);
-    final uint8List = byteData.buffer.asUint8List();
-    final widgetMarker = widget.widgetMarkers[globalKeys.indexOf(key)];
-    return Marker(
-      onTap: widgetMarker.onTap,
-      markerId: MarkerId(widgetMarker.markerId),
-      position: widgetMarker.position,
-      icon: BitmapDescriptor.fromBytes(uint8List),
-      draggable: widgetMarker.draggable,
-      infoWindow: widgetMarker.infoWindow,
-      rotation: widgetMarker.rotation,
-      visible: widgetMarker.visible,
-      zIndex: widgetMarker.zIndex,
-      onDragStart: widgetMarker.onDragStart,
-      onDragEnd: widgetMarker.onDragEnd,
-      onDrag: widgetMarker.onDrag,
-    );
+ Future<Marker> _convertToMarker(GlobalKey key) async {
+  int index = globalKeys.indexOf(key);
+  if (index == -1) {
+    // Handle the case where the key is not found in globalKeys
+    throw Exception('Key not found in globalKeys');
   }
+  final widgetMarker = widget.widgetMarkers[index];
+  RenderRepaintBoundary boundary =
+      key.currentContext!.findRenderObject()! as RenderRepaintBoundary;
+  final image = await boundary.toImage(pixelRatio: 2);
+  final byteData =
+      await image.toByteData(format: ImageByteFormat.png) ?? ByteData(0);
+  final uint8List = byteData.buffer.asUint8List();
+  return Marker(
+    onTap: widgetMarker.onTap,
+    markerId: MarkerId(widgetMarker.markerId),
+    position: widgetMarker.position,
+    icon: BitmapDescriptor.fromBytes(uint8List),
+    draggable: widgetMarker.draggable,
+    infoWindow: widgetMarker.infoWindow,
+    rotation: widgetMarker.rotation,
+    visible: widgetMarker.visible,
+    zIndex: widgetMarker.zIndex,
+    onDragStart: widgetMarker.onDragStart,
+    onDragEnd: widgetMarker.onDragEnd,
+    onDrag: widgetMarker.onDrag,
+  );
+}
+
 
   @override
   void initState() {
@@ -67,25 +73,24 @@ class _MarkerGeneratorState extends State<MarkerGenerator> {
   @override
   Widget build(BuildContext context) {
     globalKeys = [];
-    return Transform.translate(
-      /// Place markers outside of screens
-      /// To hide them in case the map becomes transparent.
-      offset: Offset(
-        -MediaQuery.of(context).size.width,
-        -MediaQuery.of(context).size.height,
-      ),
-      child: Stack(
-        children: widget.widgetMarkers.map(
-          (widgetMarker) {
-            final key = GlobalKey();
-            globalKeys.add(key);
-            return RepaintBoundary(
-              key: key,
-              child: widgetMarker.widget,
-            );
-          },
-        ).toList(),
-      ),
-    );
+return Transform.translate(
+  offset: Offset(
+    0,
+    -MediaQuery.of(context).size.height,
+  ),
+  child: Stack(
+    children: widget.widgetMarkers.map(
+      (widgetMarker) {
+        final key = GlobalKey();
+        globalKeys.add(key);
+        return RepaintBoundary(
+          key: key,
+          child: widgetMarker.widget,
+        );
+      },
+    ).toList(),
+  ),
+);
+
   }
 }
